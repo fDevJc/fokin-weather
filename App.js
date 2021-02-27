225a27c715aaf380f25d1d3bf3e6a5087684d225
fDevJc <fDevJc@gmail.com>
2021-02-27 17:14:58 +0900

    add expo-location , axios module & use weather API

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '00bd59e973f92b638eda26b2aa152fce';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+
+      console.log(`longitude:${longitude} , latitude:${latitude}`);
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Can't find you");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,22 @@
+#Fokin Weather
+노마드코더 무료강의를 따라 리액트 네이티브 앱 만들기
+
+#Use Module
+Xcode(Mac)
+expo
+expo-location(위치정보 모듈)
+axios
+
+#Use Command
+expo project
+
+> sudo npm i -g expo-cli
+> expo 프로젝트 생성 (create-react-app과 유사)
+> expo init fokin-weather
+> expo location
+> expo install expo-location
+> axios
+> npm i axios
+
+#Use API
+Weather API 날씨정보 : https://openweathermap.org/
